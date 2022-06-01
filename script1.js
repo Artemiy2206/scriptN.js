173f49c058d67556994521fb73296725fdb6b91a
Artemiy2206 <101861600+Artemiy2206@users.noreply.github.com>
2022-06-01 10:00:00 +0300

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,7 +1,7 @@
 var groceries = ['чай', 'шпроты', 'печенье', 'сахар', 'чипсы'];
 var shoppingList = '';
 
-for (var i = 0; i <= groceries.length - 2; i++) {
+for (i = 0; i <= groceries.length - 2; i++) {
     shoppingList += groceries[i] + ', ';
   }
 shoppingList += groceries[groceries.length - 1];
